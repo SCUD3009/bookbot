3b2e90f111a335983c8cae95226db0a621b2d99f
SCUD3009 <s.c.u.d@web.de>
2025-05-10 15:39:42 +0200

    get_num_character added

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from stats import get_num_words
+from stats import *
 
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(text, f"{num_words} words found in the document")
+    num_character = get_num_character(text)
+    #print(text) removed for no wall of text every time
+    print(
+        f"{num_words} words found in the document.\n", 
+        f"Used Characters:{num_character}"
+          )
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -4,3 +4,14 @@ def get_num_words(text):
     for word in words:
         num += 1
     return num
+
+
+def get_num_character(text):
+    l_text = text.lower()
+    num_character = {}
+    for char in l_text:
+        if char not in num_character:
+            num_character[char] = 1
+        else:
+            num_character[char] += 1
+    return num_character
