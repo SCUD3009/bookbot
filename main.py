673be445f5419d010d9c19db0b8df79cea1a51fb
SCUD3009 <s.c.u.d@web.de>
2025-05-11 22:05:57 +0200

    more books for testing

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from stats import *
+import sys
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_character = get_num_character(text)
